459e754157ce6447296a2eb5a6074d49822468d4
Philip Weaver <simplygreatwork@gmail.com>
2023-09-13 02:01:26 -0500

    shove

--- source/html-console.js
+++ source/html-console.js
@@ -1,22 +1,45 @@
 
+let console_log = console.log
+
+// console.log fails with a single line.
+
 export function console_init(selector) {
 	
-	let console_log = console.log
-	console.log = function() {
-		console_log(...arguments)
-		console_write(selector, ...arguments)
+	let console = { selector: selector }
+	console.log = function(message) {
+		console_log_terminal(console, message)
+		console_log_browser(console, message)
 	}
+	return console
 }
 
-function console_write(selector, message) {
+export function console_log_terminal(printer, string) {
 	
-	if (! (window && window.document)) return
-	selector = selector || body
+	let array = string.split('\n')
+	let delta = array[1].length - array[1].trimLeft().length
+	string = array.map(function(each) {
+		return each.slice(delta)
+	}).join('\n')
+	string = string.replaceAll('\t', '  ')
+	console_log(string)
+}
+
+export function console_log_browser(printer, string) {
+	
+	let array = string.split('\n')
+	let delta = array[1].length - array[1].trimLeft().length
+	string = array.map(function(each) {
+		return each.slice(delta)
+	}).join('\n')	
+		
 	var div = document.createElement('div')
 	div.style.cssText = 'font-family:mono;font-size:75%'
-	message = new String(message)
-	message = message.replaceAll(' ', '&nbsp;')
-	message = message.replaceAll('\n', '<br>')
-	div.innerHTML = message
-	document.querySelector(selector).appendChild(div)
+	string = string.replaceAll(' ', '&nbsp;')
+	string = string.replaceAll('\n', '<br>')
+	string = string.replaceAll('\t', '&nbsp;&nbsp;')
+	div.innerHTML = string
+	document.querySelector(printer.selector).appendChild(div)
 }
+
+
+
