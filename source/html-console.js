02806e08cad328f2e3b648a21ecd3b64703a9a6f
Philip Weaver <simplygreatwork@gmail.com>
2023-09-13 02:25:12 -0500

    shove

--- source/html-console.js
+++ source/html-console.js
@@ -1,45 +1,45 @@
 
 let console_log = console.log
-
-// console.log fails with a single line.
+let selector_
 
 export function console_init(selector) {
 	
-	let console = { selector: selector }
-	console.log = function(message) {
-		console_log_terminal(console, message)
-		console_log_browser(console, message)
+	selector_ = selector						// closure?
+	return {
+		log: function(message) {
+			console_log_terminal(console, message)
+			console_log_browser(console, message)
+		}
 	}
-	return console
 }
 
-export function console_log_terminal(printer, string) {
+export function console_log_terminal(console, string) {
 	
-	let array = string.split('\n')
-	let delta = array[1].length - array[1].trimLeft().length
-	string = array.map(function(each) {
-		return each.slice(delta)
-	}).join('\n')
+	string = tweak(string)
 	string = string.replaceAll('\t', '  ')
 	console_log(string)
 }
 
-export function console_log_browser(printer, string) {
+export function console_log_browser(console, string) {
 	
-	let array = string.split('\n')
-	let delta = array[1].length - array[1].trimLeft().length
-	string = array.map(function(each) {
-		return each.slice(delta)
-	}).join('\n')	
-		
-	var div = document.createElement('div')
-	div.style.cssText = 'font-family:mono;font-size:75%'
+	string = tweak(string)		
 	string = string.replaceAll(' ', '&nbsp;')
 	string = string.replaceAll('\n', '<br>')
 	string = string.replaceAll('\t', '&nbsp;&nbsp;')
+	let div
+	div = document.createElement('div')
+	div.style.cssText = 'font-family:mono;font-size:75%'
 	div.innerHTML = string
-	document.querySelector(printer.selector).appendChild(div)
+	console.selector = 'body'
+	document.querySelector(console.selector).appendChild(div)
 }
 
-
-
+function tweak(string) {
+	
+	let array = string.split('\n')
+	if (array.length < 2) return string
+	let delta = array[1].length - array[1].trimLeft().length
+	return array.map(function(each) {
+		return each.slice(delta)
+	}).join('\n')
+}
